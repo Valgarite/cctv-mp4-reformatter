f9b1e25cede59273e3aa21a32c6b523d74eb2735
Carlos Gutiérrez <carlsgutierrez259@gmail.com>
2024-02-14 14:56:05 -0400

    Working

--- text_processing.py
+++ text_processing.py
@@ -6,37 +6,38 @@ NO_TIME = "no-time"
 
 reader = easyocr.Reader(['en'])
 
-def processOCR(imgURL: str):
-    ocr_result = reader.readtext(imgURL)
-    print(ocr_result)
+def processOCR(imgURL: str)->dict[str, str, str]:
     best_date_certainty, best_time_certainty = 0 , 0
-    camera_number = None
+    best_date, best_time, found_cam = 'NO_DATE', 'NO_TIME', 'NO_CAM'
     previous_word = None
-    found_cam = 'other_cam'
-    for result in ocr_result:
-        word, certainty = result[1], result[2]
-        if previous_word == 'Camera':
-            camera_number = word
-        previous_word = word
-        check_date = is_date(word)
-        check_time = is_time(word)
-        if check_date != NO_DATE:
-            date_certainty = certainty
-        elif check_time != NO_TIME:
-            time_certainty = certainty
-        
-        if check_date != NO_DATE and date_certainty >= best_date_certainty:
-            best_date = check_date
-            best_date_certainty = date_certainty
-            print("date: ", best_date, best_date_certainty)
+    
+    try:
+        ocr_result = reader.readtext(imgURL)
+        print(ocr_result)
+        for result in ocr_result:
+            word, certainty = result[1], result[2]
+            if previous_word == 'Camera':
+                found_cam = word
+            previous_word = word
+            check_date = is_date(word)
+            check_time = is_time(word)
+            if check_date != NO_DATE:
+                date_certainty = certainty
+            elif check_time != NO_TIME:
+                time_certainty = certainty
             
-        if check_time != NO_TIME and time_certainty >= best_time_certainty:
-            best_time = check_time
-            best_time_certainty = time_certainty
-            print("time: ", best_time, best_time_certainty)
-    if camera_number==2 or camera_number == 3:
-        found_cam = camera_number
-    return f'Cam{found_cam}D{best_date}T{best_time}'
+            if check_date != NO_DATE and date_certainty >= best_date_certainty:
+                best_date = check_date
+                best_date_certainty = date_certainty
+                print("date: ", best_date, best_date_certainty)
+                
+            if check_time != NO_TIME and time_certainty >= best_time_certainty:
+                best_time = check_time
+                best_time_certainty = time_certainty
+                print("time: ", best_time, best_time_certainty)
+    except:
+        pass
+    return { 'date': best_date, 'time': best_time, 'camera': found_cam, 'best_date_certainty': best_date_certainty, 'best_time_certainty': best_time_certainty}
 
 def is_date(date_string: str) -> str:
     try:
--- video_processing.py
+++ video_processing.py
@@ -9,28 +9,39 @@ def read_video(video: cv2.VideoCapture)->str:
     frame_list = slice_frames(video=video, quant=20)
     success, image = video.read()
     scan_result = NO_DATE
+    found_cam = 'other_cam'
+    certainties = {'date': 0, 'time': 0}
     while(success):
         try:
             os.mkdir('temp')
         except:
             pass
         for each_frame in frame_list:
+            each_frame = int(each_frame)
+            video.set(cv2.CAP_PROP_POS_FRAMES, each_frame)
+            success, image = video.read()
+            imgURL = f'temp/{each_frame}.jpg'
+            each_frame = str(each_frame)
+            print(imgURL)
             try:
-                each_frame = int(each_frame)
-                video.set(cv2.CAP_PROP_POS_FRAMES, each_frame)
-                success, image = video.read()
-                imgURL = f'temp/{each_frame}.jpg'
-                each_frame = str(each_frame)
-                print(imgURL)
                 cv2.imwrite(imgURL, image)
-                scan_result = processOCR(imgURL)
-                os.remove(imgURL)
             except:
-                print(Exception)
                 pass
+            scan_result = processOCR(imgURL)
+            
+            if(scan_result['best_date_certainty'] > certainties['date']):
+                certainties['date'] = scan_result['best_date_certainty']
+                date = scan_result['date']
+            if(scan_result['best_time_certainty'] > certainties['time']):
+                certainties['time'] = scan_result['best_time_certainty']
+                clock = scan_result['time']
+            if scan_result['camera'] == "02" or scan_result['camera'] == "03":
+                found_cam = scan_result['camera']
+            print(found_cam)
         video.release()
         break
-    return scan_result
+    filename = f"camera{found_cam}-{date}_{clock}.mp4"
+    return filename
 
 def slice_frames(video: cv2.VideoCapture, quant: int = 30):
     videoLength = video.get(cv2.CAP_PROP_FRAME_COUNT)
